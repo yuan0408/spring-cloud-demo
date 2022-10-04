bd2b07a0297539abe669bcf89ae4be4a7546b969
yuan <1209129548@qq.com>
2022-10-04 15:36:53 +0800

    使用Hystrix实现服务降级和服务熔断

--- borrow-service/src/main/java/com/example/controller/BorrowController.java
+++ borrow-service/src/main/java/com/example/controller/BorrowController.java
@@ -2,11 +2,13 @@ package com.example.controller;
 
 import com.example.entity.UserBorrowDetail;
 import com.example.service.BorrowService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @RestController
 public class BorrowController {
@@ -14,8 +16,17 @@ public class BorrowController {
     @Resource
     BorrowService service;
 
+    @HystrixCommand(fallbackMethod = "onError")
     @RequestMapping("/borrow/{uid}")
     UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid){
+        System.out.println("正常调用");
         return service.getUserBorrowDetailByUid(uid);
+    }
+
+    //备选方案，这里直接返回空列表了
+    //注意参数和返回值要和上面的一致
+    UserBorrowDetail onError(int uid){
+        System.out.println("备用方案");
+        return new UserBorrowDetail(null, Collections.emptyList());
     }
 }
