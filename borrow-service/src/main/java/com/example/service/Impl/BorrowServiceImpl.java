a0ac44f968fa748c22fe969749686d6d72e7068d
yuan <1209129548@qq.com>
2022-10-04 12:18:34 +0800

    eureka简单使用

--- borrow-service/src/main/java/com/example/service/Impl/BorrowServiceImpl.java
+++ borrow-service/src/main/java/com/example/service/Impl/BorrowServiceImpl.java
@@ -18,16 +18,17 @@ public class BorrowServiceImpl implements BorrowService {
 
     @Resource
     BorrowMapper mapper;
+    @Resource
+    RestTemplate template;
 
     @Override
     public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrow = mapper.getBorrowsByUid(uid);
 
-        RestTemplate template = new RestTemplate();
-        User user = template.getForObject("http://localhost:8101/user/" + uid, User.class);
+        User user = template.getForObject("http://userService/user/" + uid, User.class);
         List<Book> bookList = borrow
                 .stream()
-                .map(b-> template.getForObject("http://localhost:8301/book/"+b.getBid(),Book.class))
+                .map(b-> template.getForObject("http://bookService/book/"+b.getBid(),Book.class))
                 .collect(Collectors.toList());
         return new UserBorrowDetail(user,bookList);
     }
