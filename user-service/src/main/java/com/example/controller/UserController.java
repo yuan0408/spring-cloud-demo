80ad9eeba2a23eb877805104c683f8ed1ca8c3fb
yuan <1209129548@qq.com>
2022-10-17 21:54:18 +0800

    使用nacos配置中心

--- user-service/src/main/java/com/example/controller/UserController.java
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import com.example.entity.User;
 import com.example.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +13,20 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class UserController {
 
     @Resource
     UserService service;
 
+    @Value("${test.name}")
+    String test;
+
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid){
         log.info("user服务被调用");
+        log.info("test: "+test);
         return service.getUserById(uid);
     }
 }
